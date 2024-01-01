19ff1b63de17804ed8f8f2d589c050d634bf0c9e
leongmichael <michaelleong1229@gmail.com>
2023-12-31 20:45:31 -0800

    functional grade fetching

--- public/content-scripts.js
+++ public/content-scripts.js
@@ -1,6 +1,7 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.message === "getGrade") {
-        const gradeElement = document.querySelector('span#grade'); // TODO: FIX 
+        const gradeElement = document.querySelector('.student_assignment.final_grade span.grade'); 
+        console.log(gradeElement);
         if (gradeElement) {
             sendResponse({ grade: gradeElement.textContent });
         } else {
